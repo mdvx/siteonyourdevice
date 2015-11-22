a0c6b6b9803eb9f4ae7066c7715c933c60b0b48d
Tatiana <binhot@tut.by>
2015-11-22 13:04:16 +0200

    Проверка корректности домена при регистрации сайта

--- function/registration_site.php
+++ function/registration_site.php
@@ -1,5 +1,4 @@
 ﻿<?php
-
 	$sait=clean($_POST['sait']); $opisanieSait=clean($_POST['opisanieSait']); 
 		if ($sait!=null)
 		{ 
@@ -7,37 +6,34 @@
 		$data = mysqli_fetch_array ($query);
 			if ($data==null)
 			{
-			$s=mysqli_query($connect, "INSERT INTO users_site SET id_users='".$_POST['hidden']."', adds_site='".$sait."', description='".$opisanieSait."' ");
-			echo "Сайт успешно зарегистрирован.";
-			echo "<br>".mysql_error();
-			
-			//узнаем массив сайтов
-			$q = mysqli_query($connect, "SELECT adds_site FROM users_site WHERE id_users='".$_POST['hidden']."'");
-			$num_results = mysqli_num_rows($q);
-			
-			$hostArray = [];
-			for ($a=0; $a<$num_results; $a++) 
+				//шаблон для проверки доменного имени сайта
+				$pattern ='#^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$#';
+				if ( preg_match($pattern,$sait) )
 				{
-				$result = mysqli_fetch_row($q);
-				array_push($hostArray, implode($result) );
-				}
-			//var_dump($hostArray);
-						
-			//запись в базу redis
-			$arr = array ('name' => $arr['name'], 'secondName' => $arr['second_name'], 'login' => $arr['login'], 'password'=>  $arr['password'], 
-			'hosts' => $hostArray );
-			$jsonData = json_encode($arr);
-			include 'redis_connect.php';
-			$key = $arr['login'];
-			$redis -> hset ('users',$key, $jsonData);
-			}
-			else { echo "Сайт с таким же доменным именем уже был зарегистрирован.";}
+					$s=mysqli_query($connect, "INSERT INTO users_site SET id_users='".$_POST['hidden']."', adds_site='".$sait."', description='".$opisanieSait."' ");
+					echo "Сайт успешно зарегистрирован.";
+					echo "<br>".mysql_error();
+					//узнаем массив сайтов
+					$q = mysqli_query($connect, "SELECT adds_site FROM users_site WHERE id_users='".$_POST['hidden']."'");
+					$num_results = mysqli_num_rows($q);
 			
-		}
-		else 
-		{ echo "Поле с доменным именем сайта пустым быть не должно.";}
+					$hostArray = array();
+					for ($a=0; $a<$num_results; $a++) 
+						{
+						$result = mysqli_fetch_row($q);
+						array_push($hostArray, implode($result) );
+						}
+					//запись в базу redis
+					$arr = array ('name' => $arr['name'], 'secondName' => $arr['second_name'], 'login' => $arr['login'], 'password'=>  $arr['password'], 
+					'hosts' => $hostArray );
+					$jsonData = json_encode($arr);
+					include 'redis_connect.php';
+					$key = $arr['login'];
+					$redis -> hset ('users',$key, $jsonData);
+				} else { echo "Указан некорректный адрес сайта.";}
+			} else { echo "Сайт с таким же доменным именем уже был зарегистрирован.";}
+		} else { echo "Поле с доменным именем сайта пустым быть не должно.";}
 
 function clean($value="")
 { $value = trim($value); $value=stripcslashes($value); $value=strip_tags($value); $value=htmlspecialchars($value); return $value; }		
-
 ?>
