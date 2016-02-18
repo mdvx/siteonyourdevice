77ce7d0e062b97057faad2a958b12320d0c5629d
topilski <atopilski@gmail.com>
2016-02-18 19:27:24 +0300

    Sources UP

--- back_end/src/main.cpp
+++ back_end/src/main.cpp
@@ -21,7 +21,7 @@
 // [-c] config path [-d] run as daemon
 
 int main(int argc, char *argv[]) {
-    fasto::siteonyourdevice::application::FastoApplication app(argc, argv);
-    int res = app.exec();
-    return res;
+  fasto::siteonyourdevice::application::FastoApplication app(argc, argv);
+  int res = app.exec();
+  return res;
 }
